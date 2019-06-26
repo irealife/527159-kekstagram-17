00adf6c6ed1f6552f84e0af70357db300da43f87
irealife <irealife@rambler.ru>
2019-06-26 21:41:57 +0300

    правка

--- js/main.js
+++ js/main.js
@@ -103,6 +103,8 @@ var scalePhoto = document.querySelector('.img-upload__scale');
 var resizeValue = scalePhoto.querySelector('.scale__control--value');
 var effectLevel = document.querySelector('.effect-level');
 
+var inputComments = document.querySelector('.text__description');
+
 function fillValues() {
   loadPicture = document.querySelector('.img-upload__preview img');
   effectVal = document.querySelector('.effect-level__value');
@@ -124,10 +126,8 @@ function fillValues() {
 }
 
 function onEditPhotoEscPress(evt) {
-  if (evt.keyCode === 27) {
-    if (document.activeElement !== document.querySelector('.text__description')) {
-      closeEditPhoto();
-    }
+  if (evt.keyCode === 27 && document.activeElement !== inputComments) {
+    closeEditPhoto();
   }
 }
 
