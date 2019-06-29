8dccc0e023a2768b3bfa449739eaaf4c6ded5f39
irealife <irealife@rambler.ru>
2019-06-29 19:22:27 +0300

    удаление editPhoto

--- js/slider.js
+++ js/slider.js
@@ -6,12 +6,23 @@
   var INDENT_LEFT = 68;
   var maxRightPx = parseInt(document.querySelector('.img-upload__wrapper').offsetLeft + INDENT_LEFT, 10);
   var minLeftPx = parseInt(minLeftPx + document.querySelector('.effect-level__line').offsetWidth, 10);
-  var dragging = window.editPhoto.dragging;
+  var dragging = false;
   var effectPin = window.form.effectPin;
   var effectDepth = window.form.effectDepth;
   var effectVal = document.querySelector('.effect-level__value');
   var effectValue = window.form.effectValue;
 
+  window.fillValues = function () {
+    window.addEventListener('mouseup', function () {
+      dragging = false;
+    });
+
+    effectPin.addEventListener('mousedown', function () {
+      dragging = true;
+    });
+
+    window.addEffectsActions();
+  };
   function handleSlider(mouseX) {
     if (dragging && mouseX >= minLeftPx && mouseX <= maxRightPx) {
       var offset = Math.floor(mouseX - minLeftPx);
@@ -23,7 +34,6 @@
       window.applyEffectDepth();
     }
   }
-
   window.addEventListener('mousemove', function (evt) {
     handleSlider(evt.clientX);
   });
