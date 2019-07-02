21c32caa8f69b8ca040192b9f5bb45a80ccf3b70
irealife <irealife@rambler.ru>
2019-07-02 23:00:19 +0300

    загрузка фото с сервера

--- js/data.js
+++ js/data.js
@@ -3,56 +3,27 @@
 // добавляет случайные изображения пользователей
 
 (function () {
-  var MAX_COMMENTS = 10;
-  var MAX_NUM_AVATAR = 6;
-  var COMMENTS = [
-    'Всё отлично!',
-    'В целом всё неплохо. Но не всё.',
-    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-  ];
-  var NAMES = [
-    'Петя',
-    'Леша',
-    'Таня',
-    'Катя',
-    'Ольга',
-    'Стас',
-    'Миша'
-  ];
-  var MIN_LIKES = 15;
-  var MAX_LIKES = 200;
+  var URL = 'https://js.dump.academy/kekstagram/data';
   var pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
-  var MAX_PHOTO = 25;
-  var picturesData = [];
+  var picturesData;
 
-  function randomInteger(min, max) {
-    var rand = min + Math.random() * (max + 1 - min);
-    rand = Math.floor(rand);
-    return rand;
-  }
-
-  function getComments() {
-    var comments = [];
-    for (var i = 0; i < randomInteger(1, MAX_COMMENTS); i++) {
-      var comment = {
-        avatar: 'img/avatar-' + randomInteger(1, MAX_NUM_AVATAR) + '.svg',
-        message: COMMENTS[randomInteger(0, COMMENTS.length - 1)],
-        name: NAMES[randomInteger(0, NAMES.length - 1)]
-      };
-      comments.push(comment);
-    }
-    return comments;
-  }
-
-  function getPictureData(i) {
-    return {
-      url: 'photos/' + i + '.jpg',
-      likes: randomInteger(MIN_LIKES, MAX_LIKES),
-      comments: getComments()
+  function getDataFromServer() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', URL, true);
+    xhr.onload = function () {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          picturesData = JSON.parse(xhr.responseText);
+          renderPictures();
+        } else {
+          console.error(xhr.statusText);
+        }
+      }
     };
+    xhr.onerror = function () {
+      console.error(xhr.statusText);
+    };
+    xhr.send();
   }
 
   function generatePicture(imageData) {
@@ -61,12 +32,6 @@
     pictureElement.querySelector('.picture__likes').textContent = imageData.likes.toString();
     pictureElement.querySelector('.picture__comments').textContent = imageData.comments.length.toString();
     return pictureElement;
-  }
-
-  function generatePicturesData() {
-    for (var i = 1; i <= MAX_PHOTO; i++) {
-      picturesData.push(getPictureData(i));
-    }
   }
 
   function renderPictures() {
@@ -78,6 +43,5 @@
     pictures.appendChild(fragment);
   }
 
-  generatePicturesData();
-  renderPictures();
+  getDataFromServer();
 })();
