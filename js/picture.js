78e7d358b006b25dfe011f62e6d240526f8338ff
irealife <irealife@rambler.ru>
2019-07-04 18:35:46 +0300

    загрузка фото с сервера как в примере

--- js/picture.js
+++ js/picture.js
@@ -1,30 +1,19 @@
 'use strict';
 
-// добавляет случайные изображения пользователей
-
 (function () {
   var URL = 'https://js.dump.academy/kekstagram/data';
-  var pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
   var picturesData;
 
-  function getDataFromServer() {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', URL, true);
-    xhr.onload = function () {
-      if (xhr.readyState === 4) {
-        if (xhr.status === 200) {
-          picturesData = JSON.parse(xhr.responseText);
-          renderPictures();
-        } else {
-          console.error(xhr.statusText);
-        }
-      }
-    };
-    xhr.onerror = function () {
-      console.error(xhr.statusText);
-    };
-    xhr.send();
-  }
+  var pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+  var onError = function (message) {
+    window.console.error(message);
+  };
+
+  var onSuccess = function (data) {
+    picturesData = data;
+    renderPictures();
+  };
 
   function generatePicture(imageData) {
     var pictureElement = pictureTemplate.cloneNode(true);
@@ -35,6 +24,7 @@
   }
 
   function renderPictures() {
+
     var fragment = document.createDocumentFragment();
     var pictures = document.querySelector('.pictures');
     picturesData.forEach(function (image) {
@@ -43,5 +33,5 @@
     pictures.appendChild(fragment);
   }
 
-  getDataFromServer();
+  window.getDataFromServer(URL, onSuccess, onError);
 })();
