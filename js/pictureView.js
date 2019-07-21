c5d53048ef600e1491430eb196cd8bd59bc60921
irealife <irealife@rambler.ru>
2019-07-21 22:10:49 +0300

    isShowPopup

--- js/pictureView.js
+++ js/pictureView.js
@@ -16,7 +16,7 @@
   var commentTemplate = document.querySelector('#comment').content.querySelector('.social__comment');
   var bigPictureCommentsLoader = bigPicture.querySelector('.social__comments-loader');
   var bigPictureCancelButton = bigPicture.querySelector('.big-picture__cancel');
-  var openPopup;
+  var isShowPopup;
 
   window.showBigPicture = function (imageData) {
     document.addEventListener('keydown', onBigPictureEscPress);
@@ -31,13 +31,12 @@
     renderComments(bigPictureCommentsStart, BIGPICTURECOMMENTSPORTION, imageData.comments);
     bigPictureCommentsCount.classList.add('visually-hidden');
     bigPictureCommentsLoader.classList.add('visually-hidden');
-    bigPicture.classList.remove('hidden');
-    togglePicturePopup(openPopup);
-    // body.classList.add('modal-open');
+    togglePicturePopup(isShowPopup);
   };
 
   function togglePicturePopup(popup) {
     body.classList.toggle('modal-open', popup);
+    bigPicture.classList.toggle('hidden', popup);
   }
 
   function renderComments(start, count, comments) {
@@ -55,9 +54,7 @@
 
   function closeBigPicture() {
     document.removeEventListener('keydown', onBigPictureEscPress);
-    bigPicture.classList.add('hidden');
-    togglePicturePopup(openPopup);
-    // body.classList.remove('modal-open');
+    togglePicturePopup(isShowPopup);
   }
 
   function onBigPictureEscPress(evt) {
