a25eb67a40496d26b80cfa83eac87d413f92404a
irealife <irealife@rambler.ru>
2019-07-24 21:44:22 +0300

    правки

--- js/pictureView.js
+++ js/pictureView.js
@@ -1,7 +1,7 @@
 'use strict';
 
 (function () {
-  var BIGPICTURECOMMENTSPORTION = 5;
+  var BIG_PICTURE_COMMENTS_PORTION = 5;
   var body = document.querySelector('body');
   var bigPicture = document.querySelector('.big-picture');
   var bigPictureImg = bigPicture.querySelector('.big-picture__img img');
@@ -25,29 +25,25 @@
     bigPictureAuthorAvatar.src = 'img/avatar-' + Math.round(0.5 + Math.random() * 6) + '.svg';
     bigPictureAuthorCaption.textContent = imageData.description;
     bigPictureAuthorLikes.textContent = imageData.likes;
-    bigPictureCommentsCountPortion.textContent = BIGPICTURECOMMENTSPORTION.toString();
+    bigPictureCommentsCountPortion.textContent = BIG_PICTURE_COMMENTS_PORTION.toString();
     bigPictureCommentsCountTotal.textContent = imageData.comments.length.toString();
     bigPictureCommentsList.textContent = '';
     bigPictureCommentsStart = 0;
     renderComments();
     bigPictureCommentsLoader.classList.remove('hidden');
+    bigPictureCommentsLoader.addEventListener('click', renderComments);
     togglePicturePopup(true);
   }
 
   function togglePicturePopup(isShow) {
     body.classList.toggle('modal-open', isShow);
     bigPicture.classList.toggle('hidden', !isShow);
-    if (isShow) {
-      bigPictureCommentsLoader.addEventListener('click', renderComments);
-    } else {
-      bigPictureCommentsLoader.removeEventListener('click', renderComments);
-    }
   }
 
   function renderComments() {
-    var tmpComments = pictureData.comments.slice(bigPictureCommentsStart, (bigPictureCommentsStart + BIGPICTURECOMMENTSPORTION));
-    var end = bigPictureCommentsStart + tmpComments.length;
-    for (var i = bigPictureCommentsStart; i < end; i++) {
+    var tmpComments = pictureData.comments.slice(bigPictureCommentsStart, (bigPictureCommentsStart + BIG_PICTURE_COMMENTS_PORTION));
+    var lastCommentNumber = bigPictureCommentsStart + tmpComments.length;
+    for (var i = bigPictureCommentsStart; i < lastCommentNumber; i++) {
       var commentTemplateClone = commentTemplate.cloneNode(true);
       var commentAvatar = commentTemplateClone.querySelector('.social__picture');
       commentAvatar.src = pictureData.comments[i].avatar;
@@ -55,8 +51,8 @@
       commentTemplateClone.querySelector('.social__text').textContent = pictureData.comments[i].message;
       bigPictureCommentsList.appendChild(commentTemplateClone);
     }
-    bigPictureCommentsCountPortion.textContent = end.toString();
-    bigPictureCommentsStart = end;
+    bigPictureCommentsCountPortion.textContent = lastCommentNumber.toString();
+    bigPictureCommentsStart = lastCommentNumber;
     if (bigPictureCommentsStart >= pictureData.comments.length) {
       bigPictureCommentsLoader.classList.add('hidden');
     }
@@ -64,6 +60,7 @@
 
   function closeBigPicture() {
     document.removeEventListener('keydown', onBigPictureEscPress);
+    bigPictureCommentsLoader.removeEventListener('click', renderComments);
     togglePicturePopup(false);
   }
 
